1b8b395e0741d30001e19e048f6084970d3ac086
Falasool <meetyou202020@gmailcom>
2023-09-20 16:02:46 +0800

    Home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,1 +1,15 @@
-<template>Home</template>
+<script setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeProduct from './components/HomeProduct.vue'
+</script>
+
+<template>
+  <HomeBanner></HomeBanner>
+  <HomeHot></HomeHot>
+  <HomeCategory></HomeCategory>
+  <HomeNew></HomeNew>
+  <HomeProduct></HomeProduct>
+</template>
